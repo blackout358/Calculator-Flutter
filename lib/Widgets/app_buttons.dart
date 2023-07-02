5bb4e6ec928abc6236912e191b2b3db79f4889af
ZaidAj11 <zaidjarrah2002@gmail.com>
2023-07-02 16:58:51 +0100

    fixing animation bug by using gesturedetetor and custom animation

--- lib/Widgets/app_buttons.dart
+++ lib/Widgets/app_buttons.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AppButtons extends StatelessWidget {
+class AppButtons extends StatefulWidget {
   final Color foregroundColor;
   final Color backgroundColor;
   final Color borderColor;
   final String text;
   final int borderRadius;
   final VoidCallback onPressed;
+  Color myColor = Colors.blue;
   IconData? icon;
   double width;
   double height;
@@ -21,42 +22,42 @@ class AppButtons extends StatelessWidget {
       this.icon,
       required this.borderRadius,
       required this.onPressed});
+
+  @override
+  State<AppButtons> createState() => _AppButtonsState();
+}
+
+class _AppButtonsState extends State<AppButtons> {
+  Color buttonColor = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        margin: EdgeInsets.all(2),
-        padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
-        // width: width,
-        // height: (MediaQuery.of(context).size.width - 30) / 4,
-        // width: (MediaQuery.of(context).size.width - 30) / 4,
-        decoration: BoxDecoration(
-
-            // color: backgroundColor,
-            gradient: const LinearGradient(
-              colors: [Color(0xFFFF5B94), Color(0xFF8441A4)],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            borderRadius: BorderRadius.circular(borderRadius.toDouble()),
-            border: Border.all(color: borderColor, width: 1.0)),
-        child: ElevatedButton(
-          onPressed: onPressed,
-          style: ElevatedButton.styleFrom(
-            minimumSize: Size.fromHeight(200),
-            backgroundColor: Colors.transparent,
-            shadowColor: Colors.transparent,
-            foregroundColor: foregroundColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                borderRadius.toDouble(),
+      child: GestureDetector(
+        onTapDown: (_) => setState(() {
+          widget.onPressed;
+          buttonColor = Colors.grey;
+        }),
+        onTapUp: (_) => setState(() {
+          buttonColor = Colors.blue;
+        }),
+        child: Container(
+          margin: EdgeInsets.all(2),
+          padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+          // width: width,
+          // height: (MediaQuery.of(context).size.width - 30) / 4,
+          // width: (MediaQuery.of(context).size.width - 30) / 4,
+          decoration: BoxDecoration(
+              color: buttonColor,
+              borderRadius:
+                  BorderRadius.circular(widget.borderRadius.toDouble()),
+              border: Border.all(color: widget.borderColor, width: 1.0)),
+          child: Center(
+            child: Text(
+              widget.text,
+              style: TextStyle(
+                fontSize: 0.04 * MediaQuery.of(context).size.height,
+                color: widget.foregroundColor,
               ),
-            ),
-          ),
-          child: Text(
-            text,
-            style: TextStyle(
-              fontSize: 0.04 * MediaQuery.of(context).size.height,
             ),
           ),
         ),
